707d3d0f67f7fefb1d93ae51cd701de631a5fcdc
Fernando Mendes <fernandobvm@gmail.com>
2020-07-03 21:09:45 -0300

    begining simulation class

--- mc.py
+++ mc.py
@@ -13,3 +13,107 @@ class Rocket:
 
     def to_ork(self,xml):
         xml.write(self.name,'utf-8')
+
+
+
+class Simulation:
+    def __init__(self,name,elev,windv,winddir,launchrod,turbulency):
+        self.name = name
+        self.elevation = elev
+        self.wind_velocity = windv
+        self.wind_direction = winddir
+        self.location = ""
+        self.launch_rod = launchrod
+        self.turbulency = turbulency
+        self.simu_path = ""
+
+    def add_simulations(self,rocket):
+        self.rocket = rocket
+        rocket_run = rocket.to_xml()
+        root_before = rocket_run.getroot()
+        simulations = root_before.find('simulations')
+
+        for elev in self.elevation:
+            for wind in self.wind_velocity:
+                for winddir in self.wind_direction:
+                        
+                    #Atributo da simulação
+                    simulation = l.ET.SubElement(simulations, 'simulation')
+                    simulation.attrib['status'] = 'cantrun'
+
+                    #Nome da simulação
+                    name = l.ET.SubElement(simulation,'name')
+                    #name.text = '{}° de elevação, sob ventos de {} m/s {}°'.format(elev, wind,winddir*180/math.pi)
+                    name.text = self.name
+
+                    #Método de resolução
+                    simulator = l.ET.SubElement(simulation,'simulator')
+                    simulator.text = 'RK4Simulator'
+
+                    #Método de cálculo
+                    calculator = l.ET.SubElement(simulation,'calculator')
+                    calculator.text = 'BarrowmanCalculator'
+
+                    #cria o ramo de simulações
+                    conditions = l.ET.SubElement(simulation, 'conditions')
+
+                    #Motor CRT
+                    configid = l.ET.SubElement(conditions,'configid')
+                    configid.text = '193878ee-d8af-46eb-9af4-e67f7149b232'
+
+                    #Tamanho do Trilho de Lançamento
+                    launchrodlength = l.ET.SubElement(conditions,'launchrodlength')
+                    launchrodlength.text = self.launch_rod
+
+                    #Ângulo de lançamento
+                    launchrodangle = l.ET.SubElement(conditions,'launchrodangle')
+                    launchrodangle.text = str(elev)
+
+                    #Direção do lançamento
+                    launchroddirection = l.ET.SubElement(conditions,'launchroddirection')
+                    #launchroddirection.text = str(launchangles[3][1])
+                    launchroddirection.text = str(0)
+
+                    #Vento Médio
+                    windaverage = l.ET.SubElement(conditions,'windaverage')
+                    windaverage.text = str(wind)
+
+                    #Turbulência do Vento
+                    windturbulence = l.ET.SubElement(conditions,'windturbulence')
+                    windturbulence.text = str(0.10)
+
+                    #Direção do Vento
+                    launchwinddirection = l.ET.SubElement(conditions,'winddirection')
+                    launchwinddirection.text = str(winddir)
+
+                    #Altitude do lugar de lançamento
+                    launchaltitude = l.ET.SubElement(conditions,'launchaltitude')
+                    launchaltitude.text = str(916.0)
+
+                    #Latitude do lugar de lançamento
+                    launchlatitude = l.ET.SubElement(conditions,'launchlatitude')
+                    launchlatitude.text = str(-15.6)
+
+                    #Longitude do lugar de lançamento
+                    launchlongitude = l.ET.SubElement(conditions,'launchlongitude')
+                    launchlongitude.text = str(-47.3)
+
+                    #Condições ambientais padrão
+                    geodeticmethod = l.ET.SubElement(conditions,'geodeticmethod')
+                    geodeticmethod.text = 'spherical'
+
+                    atmosphere = l.ET.SubElement(conditions,'atmosphere')
+                    atmosphere.attrib['model'] = 'isa'
+
+                    timestep = l.ET.SubElement(conditions,'timestep')
+                    timestep.text = '0.05'
+
+                    flightdata = l.ET.SubElement(simulation,'flightdata')
+                    flightdata.attrib['maxaltitude'] = '1000' #Valor arbitrário
+
+        rocket.to_ork(rocket_run)
+
+    def run(self):
+        l.subprocess.call(["java", "-jar", "D:/Documentos/lib/UnB/4Semestre/Capital/OpenRocketTurbo2.jar", "--runSimulations", self.rocket.file_path])
+        self.simu_path = rocket.file_path
+
